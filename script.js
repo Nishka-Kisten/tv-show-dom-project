67e334a52690f72c7abafb7fcd26464663252395
Nishka Kisten <nishka.crypto@gmail.com>
2022-09-14 17:46:17 +0200

    updated 100-300

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ let body = document.querySelector('body');
 const rootElem = document.getElementById("root"); // main div with all episodes
   let allEpisodes = getAllEpisodes(); 
   let numOfEp = allEpisodes.length;
+
   let select = document.createElement('select');
   select.id= "dropdwn";
   body.insertBefore(select, rootElem);
+
   let selectHolder = document.createElement('option');
   selectHolder.value ="";
   selectHolder.innerText ="Select Episode";
   select.appendChild(selectHolder);
+
 let input = document.createElement("input");
    input.type="search";
    input.id="search";
    input.placeholder="Search...";
    body.insertBefore(input, rootElem);
+
 let display = document.createElement('div');
 display.id = "display";
    body.insertBefore(display, rootElem);
+
 let dataFrom = document.createElement('p');
 dataFrom.innerHTML = `This data is from <a href =${"https://www.tvmaze.com/shows/82/game-of-thrones"} >TVMaze.com</a> `;
 body.appendChild(dataFrom);
+
 display.innerText = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes.`;
 const search = document.getElementById('search');
 
@@ -40,7 +46,7 @@ let g = {
            id: `${allEpisodes[i].id}`
           }; arr.push(g);
 }
-let y = arr.map((ep)=> {
+let epData = arr.map((ep)=> {
   prepended_out = (
     "0" + ep.season ).slice(-2);
    prepended_out2 = (
@@ -54,8 +60,8 @@ let y = arr.map((ep)=> {
   }) .join('');
  
 
-  rootElem.innerHTML = rootElem.innerHTML + y; 
-
+  rootElem.innerHTML = rootElem.innerHTML + epData; 
+//Search for episodes
   let episode = document.querySelector('#episode');
   search.addEventListener('keyup', (e) => {
       const searchString = e.target.value.toLowerCase();
@@ -64,12 +70,10 @@ let y = arr.map((ep)=> {
             ep.name.toLowerCase().includes(searchString) ||
            ep.summary.toLowerCase().includes(searchString)
           );
-  
-      }); 
-      //console.log(filteredEp)
+     
+      });  
       
         if(filteredEp.length > 0){
-
           display.innerText = `Displaying ${filteredEp.length}/${allEpisodes.length} episodes.`
           rootElem.innerHTML =
             filteredEp.map((ep) => { 
@@ -77,35 +81,35 @@ let y = arr.map((ep)=> {
                  "0" + ep.season ).slice(-2);
                 prepended_out2 = (
                  "0" + ep.number ).slice(-2);
-         return  `
-                   <div class="episode" id="${ep.id}">
-                       <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
-                       <img src="${ep.image}" title ="ep"></img>
-                        ${ep.summary} </div> ` 
+         return   `
+         <div class="episode" id="${ep.id}">
+             <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
+             <img src="${ep.image}" title ="ep"></img>
+              ${ep.summary} </div> ` 
                         
-         })  
+         }).join('');
        } else {
-        display.innerText = `Displaying ${filteredEp.length}/${allEpisodes.length} episodes.`
+        display.innerHTML = `Displaying ${filteredEp.length}/${allEpisodes.length} episodes.`
        return rootElem.innerHTML = 'no match found'
        
        }
       
     });
 
- let d = arr.map(x => {
+ let dropDown = arr.map(x => {
   prepended_out = (
     "0" + x.season ).slice(-2);
    prepended_out2 = (
     "0" + x.number ).slice(-2);
   return `<option value="${x.id}"> S${prepended_out}E${prepended_out2}- ${x.name}</option>`
  }).join('');
- select.innerHTML =  select.innerHTML + d;
+ select.innerHTML =  select.innerHTML + dropDown;
   
 select.addEventListener('click', (e) => {
  let selectedOpt = e.target.value;
  const opt = arr.filter((ep) => {
   return  ep.id === selectedOpt
-});  
+})
  if(opt == ""){
   display.innerText = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes.`
   return   rootElem.innerHTML =
@@ -119,8 +123,8 @@ select.addEventListener('click', (e) => {
                <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
                  <img src="${ep.image}" title ="ep"></img>
                  ${ep.summary}
-              </div>`;
-    }) .join(''); ;
+              </div>`
+    }) .join('');
  } else {
   display.innerText = `Displaying ${opt.length}/${allEpisodes.length} episodes.`
   return rootElem.innerHTML = 
@@ -134,6 +138,6 @@ select.addEventListener('click', (e) => {
         <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
         <img src="${ep.image}" title ="ep"></img>
          ${ep.summary} </div> ` 
-  })
+  }).join('')
  }
 })
