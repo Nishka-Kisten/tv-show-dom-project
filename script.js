5f54b6c4cc72273bb35f7d86944c12b1a56583e6
Nishka Kisten <nishka.crypto@gmail.com>
2022-09-16 23:02:14 +0200

    Changed to using the API

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let body = document.querySelector('body');
-const rootElem = document.getElementById("root"); // main div with all episodes
+const rootElem = document.getElementById("root"); // main div wiShows
   let allEpisodes = getAllEpisodes();  //get data from function
-  let numOfEp = allEpisodes.length;
 
-  let select = document.createElement('select'); // create dropdown 
+  let selectSeries = document.createElement('select'); // create dropdown for series
+  selectSeries.id= "dropdwnSeries";
+  body.insertBefore(selectSeries, rootElem);
+
+  let selectHoldSeries = document.createElement('option'); // create options for dropdown series
+  selectHoldSeries.value ="";
+  selectHoldSeries.innerText ="Select Series";
+  selectSeries.appendChild(selectHoldSeries);
+
+  let select = document.createElement('select'); // create dropdown for episodes
   select.id= "dropdwn";
   body.insertBefore(select, rootElem);
 
-  let selectHolder = document.createElement('option'); // create options for dropdown
+  let selectHolder = document.createElement('option'); // create options for dropdown episodes
   selectHolder.value ="";
   selectHolder.innerText ="Select Episode";
   select.appendChild(selectHolder);
@@ -22,109 +30,105 @@ let input = document.createElement("input"); //create input field
 let displayNum = document.createElement('div');  //how many episodes on the screen
 displayNum.id = "displayNum";
    body.insertBefore(displayNum, rootElem);
-   displayNum.innerText = `displaying ${allEpisodes.length}/${allEpisodes.length} episodes.`;
 
 let dataFrom = document.createElement('p'); // element telling where data is from
 dataFrom.innerHTML = `This data is from <a href =${"https://www.tvmaze.com/shows/82/game-of-thrones"} >TVMaze.com</a> `;
 body.appendChild(dataFrom);
 
 
-let arr =[];   //Creating array of data needed
-
- for(let i=0; i<numOfEp; i++){
-  // Zero pads to two digits for season and episode.
-     prepended_out = ("0" + allEpisodes[i].season ).slice(-2);
-   prepended_out2 = ("0" + allEpisodes[i].number ).slice(-2);
-let epData = {
-            name:`${allEpisodes[i].name}`,
-           season: `${prepended_out}`,
-           number: `${prepended_out2}`,
-           image: `${allEpisodes[i].image.medium}`,
-           summary: `${allEpisodes[i].summary}`,
-           id: `${allEpisodes[i].id}`
+let allShows = getAllShows();
+let length = allShows.length;
+let arr =[];
+  //Creating array of data needed
+ for(let i=0; i<length; i++){
+let g = {
+          name:`${allShows[i].name}`,
+           id: `${allShows[i].id}`
           }; 
-   arr.push(epData);
+arr.push(g);
+
 }
+// let go = arr.(x => getEpData(x.id);
 
-
-//episode data to be displayed on screen
-function display(){
-  let data = arr.map((ep)=> {
-    prepended_out = ("0" + ep.season ).slice(-2);
-     prepended_out2 = ("0" + ep.number ).slice(-2);
-    return   `
-           <div class="episode" id="${ep.id}">
-               <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
-                 <img src="${ep.image}" title ="ep"></img>
-                 ${ep.summary}
-              </div>`;
-    }) .join('');
-   return data;
+async function getEpData(SHOW_ID){
+   let url = `https://api.tvmaze.com/shows/82/episodes`;
+  // let url2 = `https://api.tvmaze.com/shows/${SHOW_ID}/episodes`;
+  try {
+    const response = await fetch(url);
+    episodeData = await response.json();
+    displayEp(episodeData)
+    makeDropDown(episodeData);
+   //displayImg(episodeData);
+} catch (err) {
+    console.error(err);
+}
   }
 
- rootElem.innerHTML = rootElem.innerHTML + display(); 
-
-  let episode = document.querySelector('#episode');
+  function displayEp(data){
+    displayNum.innerText = `displaying ${data.length}/${episodeData.length} episodes.`;
+    const results = data.map(ep => { 
+      prepended_out = ("0" + ep.season ).slice(-2);
+      prepended_out2 = ("0" + ep.number ).slice(-2);
+        return `
+        <div class="episode" id="${ep.id}">
+        <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
+        <img src="${ep.image.medium}"></img>
+          ${ep.summary}
+         </div>  
+    `;
+    }) .join('');
+    rootElem.innerHTML = results;
+  }
+  
 //Search for episodes
   search.addEventListener('keyup', (e) => {
       const searchString = e.target.value.toLowerCase(); //value typed
-      const filteredEp = arr.filter((ep) => {
+      const filteredEp = episodeData.filter((ep) => {
           return (  
             ep.name.toLowerCase().includes(searchString) || 
            ep.summary.toLowerCase().includes(searchString)
-          );
-     
-      });  
-      
-        if(filteredEp.length > 0){
-          displayNum.innerText = `Displaying ${filteredEp.length}/${allEpisodes.length} episodes.`
-          //changes inner text depending on how many episodes displayed
+          )}); 
+         displayEp(filteredEp);
+        })
 
-          rootElem.innerHTML =     //changes episodes displayed depending on input
-            filteredEp.map((ep) => { 
-                prepended_out = ("0" + ep.season ).slice(-2);
-                prepended_out2 = ( "0" + ep.number ).slice(-2);
-         return   `
-         <div class="episode" id="${ep.id}">
-             <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
-             <img src="${ep.image}" title ="ep"></img>
-              ${ep.summary} </div> ` ;
-                        
-         }).join('');
-       } else {
-        displayNum.innerHTML = `Displaying ${filteredEp.length}/${allEpisodes.length} episodes.`
-       return rootElem.innerHTML =  `<p>No match found</p>`;
-       }
-      
-    });
 
- let dropDown = arr.map(x => { //displays all episode names in dropdown
-  prepended_out = ( "0" + x.season ).slice(-2);
-  prepended_out2 = ( "0" + x.number ).slice(-2);
-    return `<option value="${x.id}"> S${prepended_out}E${prepended_out2}- ${x.name}</option>`
+  async function makeDropDown(data){
+ let dropDown = data.map(ep => { //displays all episode names in dropdown
+  prepended_out = ( "0" + ep.season ).slice(-2);
+  prepended_out2 = ( "0" + ep.number ).slice(-2);
+    return `<option value="${ep.id}"> S${prepended_out}E${prepended_out2}- ${ep.name}</option>`
  }).join('');
 
- select.innerHTML =  select.innerHTML + dropDown;
-  
+ select.innerHTML +=dropDown;
+
+ let dropDownSeries = arr.map(ep =>{ //displays series names in dropdown
+  return `<option value=${ep.id}">${ep.name}</option>`
+ }).join('');
+
+ selectSeries.innerHTML += dropDownSeries;
+         }
+        
+
+
 select.addEventListener('click', (e) => {
- let selectedOpt = e.target.value;
- const opt = arr.filter((ep) => {
-  return  ep.id === selectedOpt;
-})  //displays episode depending on what option is picked
+ let selectedOption = e.target.value;
+ 
+ const opt = episodeData.filter((ep) => {
+  return  ep.id == selectedOption;
+}); //displays episode depending on what option is picked
  if(opt == ""){ 
-  displayNum.innerText = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes.`
-  return   rootElem.innerHTML += display();
+  displayEp(episodeData);
   } else {
-  displayNum.innerText = `Displaying ${opt.length}/${allEpisodes.length} episodes.`
-  return rootElem.innerHTML = 
-  opt.map((ep)=> {
-     prepended_out = ("0" + ep.season ).slice(-2);
-     prepended_out2 = ("0" + ep.number ).slice(-2);
-    return  `
-    <div class="episode" id="${ep.id}">
-        <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
-        <img src="${ep.image}" title ="ep"></img>
-         ${ep.summary} </div> ` 
-  }).join('')
+ displayEp(opt);
  }
 })
+
+
+// selectSeries.addEventListener('click', (e) => {
+// let seriesSelected =  e.target.value;
+
+
+
+// })
+
+getEpData();
