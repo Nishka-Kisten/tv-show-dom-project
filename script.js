3742146cfe4ce1adc4b54ce2915ae2a4c41c3c7d
Nishka Kisten <nishka.crypto@gmail.com>
2022-10-07 00:57:17 +0200

    Displays all series
    All series are able to be displayed, episode selector is not fixed yet. Still displays previous series that were clicked.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const rootElem = document.getElementById("root"); // main div wiShows
   body.insertBefore(selectSeries, rootElem);
 
   let selectHoldSeries = document.createElement('option'); // create options for dropdown series
-  selectHoldSeries.value ="";
+  selectHoldSeries.value ="Series";
   selectHoldSeries.innerText ="Select Series";
   selectSeries.appendChild(selectHoldSeries);
 
@@ -16,8 +16,8 @@ const rootElem = document.getElementById("root"); // main div wiShows
   body.insertBefore(select, rootElem);
 
   let selectHolder = document.createElement('option'); // create options for dropdown episodes
-  selectHolder.value ="";
-  selectHolder.innerText ="Select Episode";
+  selectHolder.value = "episodes";
+  selectHolder.innerText = "Select Episode";
   select.appendChild(selectHolder);
 
 let input = document.createElement("input"); //create input field
@@ -38,97 +38,106 @@ body.appendChild(dataFrom);
 
 let allShows = getAllShows();
 let length = allShows.length;
-let arr =[];
+let shows =[];
   //Creating array of data needed
  for(let i=0; i<length; i++){
-let g = {
-          name:`${allShows[i].name}`,
-           id: `${allShows[i].id}`
-          }; 
-arr.push(g);
-
+let g = { name:`${allShows[i].name}`, id: `${allShows[i].id}` }; 
+shows.push(g);
 }
-// let go = arr.(x => getEpData(x.id);
-
-async function getEpData(SHOW_ID){
-   let url = `https://api.tvmaze.com/shows/82/episodes`;
-  // let url2 = `https://api.tvmaze.com/shows/${SHOW_ID}/episodes`;
-  try {
-    const response = await fetch(url);
-    episodeData = await response.json();
-    displayEp(episodeData)
-    makeDropDown(episodeData);
-   //displayImg(episodeData);
-} catch (err) {
-    console.error(err);
-}
+const arr = shows.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); 
+  if (nameA < nameB) {
+    return -1; //nameA comes first
   }
+  if (nameA > nameB) {
+    return 1; // nameB comes first
+  }
+  return 0;  // names must be equal
+});
 
-  function displayEp(data){
-    displayNum.innerText = `displaying ${data.length}/${episodeData.length} episodes.`;
+selectSeries.addEventListener('click', (e) => {
+    let targetedShow = e.target.value;
+      var filteredArr = arr.filter((show) => { return show.id.includes(targetedShow)});   
+      let SHOW_ID =  targetedShow;
+      if(filteredArr == ""){ 
+      console.log("working");
+     } else {
+    let url = `https://api.tvmaze.com/shows/${SHOW_ID}/episodes`;
+    getEpData(url);
+    }
+     })
+
+function setup() {
+  let url =`https://api.tvmaze.com/shows/82/episodes`;
+  getEpData(url);
+  let dropDownSeries = arr.map(ep =>{
+    return `<option value=${ep.id}>${ep.name}</option>`}).join('');
+ selectSeries.innerHTML += dropDownSeries
+
+}
+window.onload = setup;
+
+ async function getEpData(url){
+  fetch(url)
+   .then(response => {
+    return response.json()})
+    .then((episodeData) => {
+    displayEp(episodeData);
+    searchBar(episodeData);
+    makeDropDown(episodeData);
+    })
+ .catch (err =>{
+    console.error(err)}
+   )}
+ 
+     function displayEp(data){
+    displayNum.innerText = `displaying ${data.length}/${data.length} episodes.`;
     const results = data.map(ep => { 
       prepended_out = ("0" + ep.season ).slice(-2);
       prepended_out2 = ("0" + ep.number ).slice(-2);
+      if(ep.image == null){
+        return `
+        <div class="episode" id="${ep.id}">
+        <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
+          ${ep.summary}
+         </div> `
+        } else {
         return `
         <div class="episode" id="${ep.id}">
         <h1>${ep.name} S${prepended_out}E${prepended_out2}</h1>
         <img src="${ep.image.medium}"></img>
           ${ep.summary}
-         </div>  
-    `;
-    }) .join('');
+         </div> `}}).join('');
     rootElem.innerHTML = results;
   }
-  
-//Search for episodes
-  search.addEventListener('keyup', (e) => {
-      const searchString = e.target.value.toLowerCase(); //value typed
-      const filteredEp = episodeData.filter((ep) => {
-          return (  
-            ep.name.toLowerCase().includes(searchString) || 
-           ep.summary.toLowerCase().includes(searchString)
-          )}); 
-         displayEp(filteredEp);
-        })
 
+function searchBar(data){
+  search.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase(); //value typed
+    const filteredEp = data.filter((ep) => {
+        return (  
+          ep.name.toLowerCase().includes(searchString) || 
+         ep.summary.toLowerCase().includes(searchString)
+        )}); 
+       displayEp(filteredEp);
+      })
+}
 
   async function makeDropDown(data){
- let dropDown = data.map(ep => { //displays all episode names in dropdown
+  let dropDown = data.map(ep => {//displays all episode names in dropdown
   prepended_out = ( "0" + ep.season ).slice(-2);
   prepended_out2 = ( "0" + ep.number ).slice(-2);
     return `<option value="${ep.id}"> S${prepended_out}E${prepended_out2}- ${ep.name}</option>`
  }).join('');
-
- select.innerHTML +=dropDown;
-
- let dropDownSeries = arr.map(ep =>{ //displays series names in dropdown
-  return `<option value=${ep.id}">${ep.name}</option>`
- }).join('');
-
- selectSeries.innerHTML += dropDownSeries;
-         }
+ select.innerHTML += dropDown;
         
-
-
 select.addEventListener('click', (e) => {
- let selectedOption = e.target.value;
- 
- const opt = episodeData.filter((ep) => {
-  return  ep.id == selectedOption;
-}); //displays episode depending on what option is picked
+ let selectedOption = e.target.value; //displays episode depending on what option is picked
+  opt = data.filter((ep) => {return  ep.id == selectedOption }); 
  if(opt == ""){ 
-  displayEp(episodeData);
+  displayEp(data);
   } else {
  displayEp(opt);
  }
-})
-
-
-// selectSeries.addEventListener('click', (e) => {
-// let seriesSelected =  e.target.value;
-
-
-
-// })
-
-getEpData();
+})}
